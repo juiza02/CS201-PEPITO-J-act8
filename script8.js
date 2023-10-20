ea4ee139c4fe43984ab87e453b9677361db36198
juiza02 <145626082+juiza02@users.noreply.github.com>
2023-10-20 14:24:48 +0800

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,21 @@
+//Favorite Numbers
+const favSingers = ['Kaye Cal', 'Kenshi Yonezu', 'Yaosobi']; 
+//statement1
+ console.log(favSingers); 
+ 
+//Favorite Numbers  
+ const favNumbers = ['10', '14', '1', '2']; 
+ //statement2
+ console.log(favNumbers); 
+  
+ const mixedArr = ["string",["otherarray"], 123, true]; 
+  
+ console.log(favSingers[0]); 
+  
+ console.log(favSingers[1]); 
+  
+ console.log(favSingers[2]); 
+  
+ console.log(favNumbers[1]); 
+  
+ console.log(mixedArr[3]);
